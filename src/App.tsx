ea530a0129c2c899c16e03eaa4f29242790d0eb2
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-03-14 19:03:50 -0300

    created some animation of title page and paragraphs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {ScrollTrigger, SplitText} from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar.tsx";
+import Hero from "./components/Hero.tsx";
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -8,6 +10,7 @@ const App = () => {
     return (
         <main>
             <Navbar />
+            <Hero />
         </main>
     )
 }
